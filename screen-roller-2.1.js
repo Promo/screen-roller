a582170c43780046eab72f99da62138bb2dc4402
Крохин Руслан <ulitcos@gmail.com>
2015-09-06 18:23:30 +0500

    base for influx animate

--- screen-roller-2.1.js
+++ screen-roller-2.1.js
@@ -80,10 +80,11 @@
 
         if(nextScreen >= 0) {
             speed = (speed >= 0) ? speed : this.roller.animationSpeed;
-            this.roller.currentScreen = nextScreen;
 
+            this.roller.currentScreen = nextScreen;
             animateFunction = methods.selectAnimate.call(this);
             animateFunction.call(this, nextScreen, speed);
+
         }
     };
 
@@ -124,6 +125,22 @@
         return nextIndexScreen;
     };
 
+    methods.selectAnimate = function() {
+        return methods.selectPropertyAnimate.call(this);
+    };
+
+    methods.selectPropertyAnimate = function() {
+        return methods.move3d
+    };
+
+}(jQuery));
+
+(function($){
+    //support base animate
+    var methods = $.fn.screenroller.prototype;
+
+    methods.build.baseAnimate = true;
+
     methods.move3d = function(index, speed) {
         var $self = this;
         this.roller.beforeMove(index);
@@ -136,15 +153,6 @@
             $self.trigger('changeCurrentScreen', {index: index});
         });
     };
-
-    methods.selectAnimate = function() {
-        return methods.selectPropertyAnimate.call(this);
-    };
-
-    methods.selectPropertyAnimate = function() {
-        return methods.move3d
-    };
-
 }(jQuery));
 
 (function($){
@@ -171,129 +179,170 @@
     };
 }(jQuery));
 
-//support solid mod
 (function($){
+    //support influx animate
     var methods = $.fn.screenroller.prototype;
 
-    methods.build.solidMod = true;
+    methods.build.influxAnimate = true;
 
-    methods.solidModInit = function() {
-        methods.determineMod.call(this);
-        methods.bindChangeMod.call(this);
+    methods.influxAnimateInit = function() {
+        this.roller.$cloneScreens = []
     };
 
-    methods.determineMod = function() {
-        var $win = $(window),
-            mod;
+    methods.move3d = function(index, speed) {
+            
+        var c = this,
+            indexCurrentScreen = c.roller.currentScreen,
+            $animateScreen = c.roller.$screens.eq(index).clone(),
+            transformPrefix = c.roller.transformPrefix;
 
-        mod = (($win.height() > this.roller.minHeight) &&
-               ($win.width()  > this.roller.minWidth)) ?  'screen' : 'solid';
+        $animateScreen.addClass('clone');
+        this.append($animateScreen);
 
-        if(mod !== this.roller.mod) {
-            this.roller.mod = mod;
-            methods.setMod[mod].call(this);
-        }
-    };
+        this.roller.$cloneScreens.push($animateScreen);
 
-    methods.bindChangeMod = function() {
-        var $self = this;
+        $animateScreen.css('transition-duration', speed / 1000 + 's');
+        setTimeout(function() {
+            $animateScreen.css(transformPrefix, 'translate3d(0, 0, 0)');
+        }, 100);
 
-        $(window).on('resize.roller', function() {
-            methods.determineMod.call($self);
+        $animateScreen.one('transitionend webkitTransitionEnd', function() {
+            c.roller.$cloneScreens[0].hide();
+            // в webkit дергает экран при удалении нод
+            // используем timeout для фикса этого бага
+            setTimeout(function() {
+                c.roller.$cloneScreens[0].remove();
+                c.roller.$cloneScreens.shift();
+            }, 1000);
+
+            c.trigger('changeCurrentScreen', {index: index});
         });
     };
-
-    methods.setMod = {};
-
-    methods.setMod['screen'] = function() {
-        this.removeClass(this.roller.solidPageClass);
-
-        this.moveTo(this.roller.currentScreen, 0);
-
-        methods.removeScrollListener();
-
-        this.roller.onScreenMod();
-    };
-
-    methods.setMod['solid'] = function() {
-        this.addClass(this.roller.solidPageClass);
-
-        this.attr('style', '');
-
-        this.moveTo(this.roller.currentScreen, 0);
-
-        methods.spotOffsetsScreens.call(this);
-        methods.addScrollListener.call(this);
-
-        this.roller.onSolidMod();
-    };
-
-    methods.moveScrollTop = function(index, speed) {
-        var $self = this,
-            scrollTop = this.roller.$screens.eq(this.roller.currentScreen).offset().top;
-
-        this.roller.beforeMove(index);
-        $('html, body').stop(false, false);
-        methods.removeScrollListener.call(this);
-        $('html, body').animate({scrollTop: scrollTop}, speed, function() {
-            methods.addScrollListener.call($self);
-            $self.trigger('changeCurrentScreen', {index: index});
-        });
-    };
-
-    methods.addScrollListener = function() {
-        var $self = this;
-        $(window).on('scroll.screenroller', function() {
-            methods.checkNearScreen.call($self)
-        });
-    };
-
-    methods.removeScrollListener = function() {
-        $(window).off('scroll.screenroller');
-    };
-
-    methods.checkNearScreen = function() {
-        var nearestToCentre = methods.spotNearScreen.call(this);
-
-        if (nearestToCentre !== this.roller.currentScreen) {
-            this.roller.currentScreen = nearestToCentre;
-            this.roller.changeScreen(nearestToCentre);
-            this.trigger('changeCurrentScreen', {index: nearestToCentre});
-        }
-    };
-
-    methods.spotNearScreen = function() {
-
-        var windowScrollTop = $(window).scrollTop(),
-            nearValue = Infinity,
-            nearest;
-
-        $.each(this.roller.offsetScreens, function(index, val) {
-            if(Math.abs(windowScrollTop - val) < nearValue) {
-                nearValue = windowScrollTop - val;
-                nearest = index;
-            }
-        });
-        return nearest;
-    };
-
-    methods.spotOffsetsScreens = function() {
-        var $self = this;
-
-        this.roller.offsetScreens = [];
-        this.roller.$screens.each(function() {
-            $self.roller.offsetScreens.push($(this).offset().top);
-        });
-    };
-
-    methods.selectAnimate = function(index, speed) {
-        if(this.roller.mod === 'screen') {
-            return methods.selectPropertyAnimate.call(this, index, speed);
-        } else {
-            return methods.moveScrollTop;
-        }
-    }
 }(jQuery));
+
+//support solid mod
+//(function($){
+//    var methods = $.fn.screenroller.prototype;
+//
+//    methods.build.solidMod = true;
+//
+//    methods.solidModInit = function() {
+//        methods.determineMod.call(this);
+//        methods.bindChangeMod.call(this);
+//    };
+//
+//    methods.determineMod = function() {
+//        var $win = $(window),
+//            mod;
+//
+//        mod = (($win.height() > this.roller.minHeight) &&
+//               ($win.width()  > this.roller.minWidth)) ?  'screen' : 'solid';
+//
+//        if(mod !== this.roller.mod) {
+//            this.roller.mod = mod;
+//            methods.setMod[mod].call(this);
+//        }
+//    };
+//
+//    methods.bindChangeMod = function() {
+//        var $self = this;
+//
+//        $(window).on('resize.roller', function() {
+//            methods.determineMod.call($self);
+//        });
+//    };
+//
+//    methods.setMod = {};
+//
+//    methods.setMod['screen'] = function() {
+//        this.removeClass(this.roller.solidPageClass);
+//
+//        this.moveTo(this.roller.currentScreen, 0);
+//
+//        methods.removeScrollListener();
+//
+//        this.roller.onScreenMod();
+//    };
+//
+//    methods.setMod['solid'] = function() {
+//        this.addClass(this.roller.solidPageClass);
+//
+//        this.attr('style', '');
+//
+//        this.moveTo(this.roller.currentScreen, 0);
+//
+//        methods.spotOffsetsScreens.call(this);
+//        methods.addScrollListener.call(this);
+//
+//        this.roller.onSolidMod();
+//    };
+//
+//    methods.moveScrollTop = function(index, speed) {
+//        var $self = this,
+//            scrollTop = this.roller.$screens.eq(this.roller.currentScreen).offset().top;
+//
+//        this.roller.beforeMove(index);
+//        $('html, body').stop(false, false);
+//        methods.removeScrollListener.call(this);
+//        $('html, body').animate({scrollTop: scrollTop}, speed, function() {
+//            methods.addScrollListener.call($self);
+//            $self.trigger('changeCurrentScreen', {index: index});
+//        });
+//    };
+//
+//    methods.addScrollListener = function() {
+//        var $self = this;
+//        $(window).on('scroll.screenroller', function() {
+//            methods.checkNearScreen.call($self)
+//        });
+//    };
+//
+//    methods.removeScrollListener = function() {
+//        $(window).off('scroll.screenroller');
+//    };
+//
+//    methods.checkNearScreen = function() {
+//        var nearestToCentre = methods.spotNearScreen.call(this);
+//
+//        if (nearestToCentre !== this.roller.currentScreen) {
+//            this.roller.currentScreen = nearestToCentre;
+//            this.roller.changeScreen(nearestToCentre);
+//            this.trigger('changeCurrentScreen', {index: nearestToCentre});
+//        }
+//    };
+//
+//    methods.spotNearScreen = function() {
+//
+//        var windowScrollTop = $(window).scrollTop(),
+//            nearValue = Infinity,
+//            nearest;
+//
+//        $.each(this.roller.offsetScreens, function(index, val) {
+//            if(Math.abs(windowScrollTop - val) < nearValue) {
+//                nearValue = windowScrollTop - val;
+//                nearest = index;
+//            }
+//        });
+//        return nearest;
+//    };
+//
+//    methods.spotOffsetsScreens = function() {
+//        var $self = this;
+//
+//        this.roller.offsetScreens = [];
+//        this.roller.$screens.each(function() {
+//            $self.roller.offsetScreens.push($(this).offset().top);
+//        });
+//    };
+//
+//    methods.selectAnimate = function(index, speed) {
+//        if(this.roller.mod === 'screen') {
+//            return methods.selectPropertyAnimate.call(this, index, speed);
+//        } else {
+//            return methods.moveScrollTop;
+//        }
+//    }
+//}(jQuery));
 
 
 (function($){
