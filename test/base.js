148277838edaccd21f93068fad957e6ecbe701c6
Крохин Руслан <ulitcos@gmail.com>
2015-09-24 22:41:22 +0500

    new structure

--- test/base.js
+++ test/base.js
@@ -1,47 +1,47 @@
 //require('should');
 
-var webdriver = require('browserstack-webdriver');
-
-test = require('browserstack-webdriver/testing');
-
-test.describe('Basic tests', function() {
-    var driver, server;
-
-    test.before(function() {
-        var capabilities = {
-            'browserName' : 'firefox',
-            'browserstack.user' : 'antongrischenko1',
-            'browserstack.key' : '3coTyyD6KwNzVKzjEYas',
-            'browserstack.debug' : true,
-            'resolution' : '1024x768'
-        };
-
-        driver = new webdriver.Builder().
-            usingServer('http://hub.browserstack.com/wd/hub').
-            withCapabilities(capabilities).
-            build();
-
-        driver.get('http://promo.github.io/screen-roller/');
-    });
-
-    test.it('One scrollTop change', function() {
-        driver.manage().window().maximize();
-        driver.wait(function(){
-            driver.executeScript('$(window).scrollTop(600)');
-
-            return driver.wait(function(){
-                return function(){
-                    driver.executeScript('$(window).scrollTop(2100)');
-                    return driver.wait(function(){
-                        return driver.executeScript('return roller.currentScreen')
-                            .then(function(currentScreen){
-                                return currentScreen == 2;
-                            });
-                    }, 1000);
-                };
-            }, 1000);
-        }, 1000);
-    });
-
-    test.after(function() { driver.quit(); });
-});
+//var webdriver = require('browserstack-webdriver');
+//
+//test = require('browserstack-webdriver/testing');
+//
+//test.describe('Basic tests', function() {
+//    var driver, server;
+//
+//    test.before(function() {
+//        var capabilities = {
+//            'browserName' : 'firefox',
+//            'browserstack.user' : 'antongrischenko1',
+//            'browserstack.key' : '3coTyyD6KwNzVKzjEYas',
+//            'browserstack.debug' : true,
+//            'resolution' : '1024x768'
+//        };
+//
+//        driver = new webdriver.Builder().
+//            usingServer('http://hub.browserstack.com/wd/hub').
+//            withCapabilities(capabilities).
+//            build();
+//
+//        driver.get('http://promo.github.io/screen-roller/');
+//    });
+//
+//    test.it('One scrollTop change', function() {
+//        driver.manage().window().maximize();
+//        driver.wait(function(){
+//            driver.executeScript('$(window).scrollTop(600)');
+//
+//            return driver.wait(function(){
+//                return function(){
+//                    driver.executeScript('$(window).scrollTop(2100)');
+//                    return driver.wait(function(){
+//                        return driver.executeScript('return roller.currentScreen')
+//                            .then(function(currentScreen){
+//                                return currentScreen == 2;
+//                            });
+//                    }, 1000);
+//                };
+//            }, 1000);
+//        }, 1000);
+//    });
+//
+//    test.after(function() { driver.quit(); });
+//});
