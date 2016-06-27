45f8cf09bf39182b1388220cf52b7082b45c5000
Крохин Руслан <ulitcos@gmail.com>
2016-06-27 13:39:36 +0500

    added module arrows

--- screen-roller.js
+++ screen-roller.js
@@ -64,8 +64,9 @@
 		"./control/wheel/wheel.js": 12,
 		"./core/core.css": 13,
 		"./core/core.js": 15,
-		"./navigation/hash/hash.js": 16,
-		"./navigation/menu/menu.js": 17
+		"./navigation/arrows/arrow.js": 16,
+		"./navigation/hash/hash.js": 17,
+		"./navigation/menu/menu.js": 18
 	};
 	function webpackContext(req) {
 		return __webpack_require__(webpackContextResolve(req));
@@ -1385,6 +1386,109 @@
 /***/ function(module, exports) {
 
 	(function($) {
+	    var MODULE_NAME = 'arrows';
+	    var PLAGIN_NAME = 'screenroller' + '-' + MODULE_NAME;
+	    var MODULE_TYPE = 'navigation';
+	    var EVENT_CLICK = 'click.' + MODULE_NAME;
+	    var EVENT_REQUEST_MOVE = 'request-move';
+	    var EVENT_MOVE_SCREEN = 'move-screen';
+
+	    var DEFAULT_OPTIONS = {
+	        next: [ ],
+	        prev: [ ],
+	        classDisable: 'disable'
+	    };
+
+	    var _addListeners = function() {
+	        var module = this;
+	        var core = this.core;
+	        var $el = this.core.$el;
+
+	        module.$next.on(EVENT_CLICK, function() {
+	            if(module.enabled) {
+	                $el.trigger(EVENT_REQUEST_MOVE, {
+	                    direction: 'next',
+	                    type: MODULE_NAME,
+	                    unique: module.core.unique
+	                });
+	            }
+	        });
+
+	        module.$prev.on(EVENT_CLICK, function() {
+	            if(module.enabled) {
+	                $el.trigger(EVENT_REQUEST_MOVE, {
+	                    direction: 'prev',
+	                    type: MODULE_NAME,
+	                    unique: module.core.unique
+	                });
+	            }
+	        });
+
+	        $el.on(EVENT_MOVE_SCREEN, function(e, data){
+	            if(module.enabled && data.unique === module.core.unique) {
+
+	                module.$next.removeClass(module.classDisable);
+	                module.$prev.removeClass(module.classDisable);
+
+	                if(data.index === 0) {
+	                    module.$prev.addClass(module.classDisable);
+	                }
+
+	                if(data.index === core.countScreens - 1) {
+	                    module.$next.addClass(module.classDisable);
+	                }
+	            }
+	        });
+	    };
+
+	    var _removeListeners = function() {
+	        this.$next.off(EVENT_CLICK);
+	        this.$prev.off(EVENT_CLICK);
+	    };
+
+	    function Keyboard(params) {
+	        var options = $.extend(true, {}, DEFAULT_OPTIONS, params.options);
+
+	        this.core = params.roller;
+	        this.$next = options.next;
+	        this.$prev = options.prev;
+	        this.classDisable = options.classDisable;
+	        this.type = MODULE_TYPE;
+	        this.enable();
+
+	        _addListeners.call(this);
+	    }
+
+	    Keyboard.prototype.disable = function() {
+	        this.enabled = false;
+	    };
+
+	    Keyboard.prototype.enable = function() {
+	        this.enabled = true;
+	    };
+
+	    Keyboard.prototype.destruct = function() {
+	        _removeListeners.call(this);
+	        this.disable();
+	        delete this.core.modules[ MODULE_NAME ];
+	    };
+
+	    $.fn[ PLAGIN_NAME ] = function(options) {
+	        this.roller.modules[ MODULE_NAME ] = new Keyboard({
+	            $el: this,
+	            roller: this.roller,
+	            options: options
+	        });
+
+	        return this;
+	    };
+	}(jQuery));
+
+/***/ },
+/* 17 */
+/***/ function(module, exports) {
+
+	(function($) {
 	    var MODULE_NAME = 'hash';
 	    var PLAGIN_NAME = 'screenroller' + '-' + MODULE_NAME;
 	    var MODULE_TYPE = 'navigation';
@@ -1511,7 +1615,7 @@
 	}(jQuery));
 
 /***/ },
-/* 17 */
+/* 18 */
 /***/ function(module, exports) {
 
 	(function($) {
